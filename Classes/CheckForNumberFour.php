750c8462c37f7517cc0ae30ff3b08554b9fe9b6e
Vaughn <alvaradovaughn@yahoo.com>
2016-02-18 14:02:28 -0600

    fibbonaci

--- Classes/CheckForNumberFour.php
+++ Classes/CheckForNumberFour.php
@@ -1,7 +1,5 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: vaughn
- * Date: 2/18/16
- * Time: 1:52 PM
- */
+class checkForNumberFour
+{
+    
+}
--- tests/TestCheckForNumberFour.php
+++ tests/TestCheckForNumberFour.php
@@ -1,7 +1,7 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: vaughn
- * Date: 2/18/16
- * Time: 1:52 PM
- */
+use sprov03\WordWrap\WordWrap as myClass;
+require_once dirname(__FILE__) . '/../Classes/Fibonacci.php';
+class FibonacciTest extends PHPUnit_Framework_TestCase
+{
+
+}
