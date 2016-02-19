a145fc52760ed37d23246667fe6c4ee265446772
Vaughn <alvaradovaughn@yahoo.com>
2016-02-19 02:50:21 -0600

    tests must end with test.php

--- tests/Test.php
+++ tests/Test.php
@@ -1,6 +1,5 @@
 <?php
-use sprov03\WordWrap\WordWrap as myClass;
-require_once dirname(__FILE__) . '/../Classes/Fibonacci.php';
+
 class FibonacciTest extends PHPUnit_Framework_TestCase
 {
 
@@ -11,28 +10,28 @@ class FibonacciTest extends PHPUnit_Framework_TestCase
     {
         $this->fib = new Fibonacci();
     }
-//    function testIfOneIsAFibonacciNumber()
-//    {
-//        $this->assertTrue($this->fib->isFibonnaci(1));
-//    }
-//    function testIfTwoIsAFibonacciNumber()
-//    {
-//        $this->assertTrue($this->fib->isFibonnaci(2));
-//    }
-//    function testIfThreeIsAFibonnaciNumber()
-//    {
-//        $this->assertTrue($this->fib->isFibonnaci(3));
-//    }
-//    function testFourShouldNotBeFibonnaciNumber()
-//    {
-//        $this->assertFalse($this->fib->isFibonnaci(4));
-//    }
-//    function testEightNineIsFibonnaciNumber()
-//    {
-//        $this->assertTrue($this->fib->isFibonnaci(89));
-//    }
-//    function testSuperHighNumberShouldBeFalse()
-//    {
-//        $this->assertFalse($this->fib->isFibonnaci(1034583495));
-//    }
+    function testIfOneIsAFibonacciNumber()
+    {
+        $this->assertTrue($this->fib->isFibonnaci(1));
+    }
+    function testIfTwoIsAFibonacciNumber()
+    {
+        $this->assertTrue($this->fib->isFibonnaci(2));
+    }
+    function testIfThreeIsAFibonnaciNumber()
+    {
+        $this->assertTrue($this->fib->isFibonnaci(3));
+    }
+    function testFourShouldNotBeFibonnaciNumber()
+    {
+        $this->assertFalse($this->fib->isFibonnaci(4));
+    }
+    function testEightNineIsFibonnaciNumber()
+    {
+        $this->assertTrue($this->fib->isFibonnaci(89));
+    }
+    function testSuperHighNumberShouldBeFalse()
+    {
+        $this->assertFalse($this->fib->isFibonnaci(1034583495));
+    }
 }
