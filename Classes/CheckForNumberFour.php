69ee0bca752fca4c91e7ddc56a2f24b8af459585
Vaughn <alvaradovaughn@yahoo.com>
2016-02-22 16:19:37 -0600

    using Data provider to reduce repeating

--- Classes/CheckForNumberFour.php
+++ Classes/CheckForNumberFour.php
@@ -1,5 +1,27 @@
 <?php
 class checkForNumberFour
 {
-    return true;
+    function countDigit4In($number) {
+        $counter = 0;
+        for($i = 0; $i <= $number; $i++) {
+            $counter = $this->countDigitFour(str_split($i), $counter);
+        }
+        return $counter;
+    }
+
+    /**
+     * @param $splitNumber
+     * @param $number
+     * @return mixed
+     */
+    public function countDigitFour(array $splitNumber, $number)
+    {
+        foreach ($splitNumber as $seperatedNumber) {
+            if ($seperatedNumber == 4) {
+                $number++;
+                break;
+            }
+        }
+        return $number;
+    }
 }
--- tests/CheckForNumberFourTest.php
+++ tests/CheckForNumberFourTest.php
@@ -7,4 +7,34 @@ class CheckForNumberFourTest extends PHPUnit_Framework_TestCase
     {
         $this->num = new CheckForNumberFour();
     }
+
+    /**
+     * @dataProvider numbersWithDigit4Generator
+     */
+    function testToCheckHowManyNumber4DigitsInNumber($number, $countOfDigits4)
+    {
+        $this->assertEquals($this->num->countDigit4In($number), $countOfDigits4);
+    }
+
+    function numbersWithDigit4Generator()
+    {
+        return [
+            [10, 1],
+            [4, 1],
+            [3,0],
+            [328, 60]
+        ];
+    }
+    /*function testLoop1Through3Count0Digit4()
+    {
+        $this->assertEquals($this->num->countDigit4In(3), 0);
+    }
+    function testLoop1Through14ShouldCount2Digit4()
+    {
+        $this->assertEquals($this->num->countDigit4In(14), 2);
+    }
+    function testLoop1Through328ShouldCount60Digit4()
+    {
+        $this->assertEquals($this->num->countDigit4In(328), 60);
+    }*/
 }
